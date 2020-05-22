4f33d45fe2ae251a5019d73fe81013a198079809
Gustavo Arend Dill <gustavoarenddill42@gmail.com>
2020-05-21 23:59:56 -0300

    Project push with working set

---
+++ Android TPC Connector/Android TPC Connector.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using AndroidExtendedCommands.Permissions;
+
+namespace Android_TPC_Connector.Droid
+{
+    [Activity(Label = "Android_TPC_Connector", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            ph = new PermissionHandler(this, new string[]
+            {
+                Manifest.Permission.Internet
+            });
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            MainPage.AppContext = this;
+            ph.RequestAllPermissions();
+        }
+        PermissionHandler ph;
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            ph.RequestPermissionResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Android TPC Connector/Android TPC Connector/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Android.App;
+using AndroidExtendedCommands.CSharp.Web.Communication;
+using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace Android_TPC_Connector
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public static Activity AppContext;
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            Guid = AndroidExtendedCommands.CSharp.Generate.GUID();
+        }
+        TCPClient client;
+        Thread t;
+        string Guid = "";
+        string id = "";
+        private void btncnt_Clicked(object sender, EventArgs e)
+        {
+            t?.Abort();
+            switch (btncnt.Text)
+            {
+                case "Connect":
+                    client = new TCPClient(ip.Text, ushort.Parse(port.Text));
+                    btncnt.Text = "Disconnect";
+                    smsg.Text = "";
+                    try
+                    {
+                        try { client.Connect(); } catch (Exception ex) { smsg.Text = ex.Message; }
+                        Thread.Sleep(500);
+                        if (client.Connected)
+                        {
+                            t = new Thread(new ThreadStart(ListenToServer));
+                            t.Start();
+                        }
+                    }
+                    catch { smsg.Text = "Connection Refused!"; btncnt.Text = "Connect"; }
+                    break;
+                case "Disconnect":
+                    btncnt.Text = "Connect";
+                    t.Abort();
+                    client.SendString("ClientDisconnected-ID:" + id);
+                    client.Disconnect();
+                    break;
+            }
+        }
+        void ListenToServer()
+        {
+            while (t.IsAlive && client.Connected)
+            {
+                do
+                {
+                    if (!client.Connected)
+                        break;
+                    var cnt = client.ReceiveString();
+                    if (Regex.IsMatch(cnt, "AssignID: \"(\\d+)\""))
+                    {
+                        var id = Regex.Match(cnt, "AssignID: \"(\\d+)\"").Groups[1].Value;
+                        this.id = id;
+                        var welcomeMsg = cnt.Substring(("AssignID: \"" + id + "\" WelcomeMsg: \"").Length);
+                        welcomeMsg.Substring(0, welcomeMsg.Length - 1);
+                        Device.BeginInvokeOnMainThread(delegate ()
+                        {
+                            idlbl.Text = "Id: " + id;
+                            smsg.Text = welcomeMsg.Replace("\\n", "\n").Replace("\\t", "\t");
+                        });
+                        break;
+                    }
+                    switch (cnt)
+                    {
+                        case "server-guid-request":
+                            client.SendString(Guid);
+                            break;
+                        default:
+                            Device.BeginInvokeOnMainThread(delegate ()
+                            {
+                                smsg.Text = cnt;
+                            });
+                            break;
+                    }
+                } while (false);
+            }
+        }
+    }
+}
